93e060dc6b0344f92bf3d9598ba104396a8958ca
simrink <simrinkalkat@gmail.com>
2023-03-05 16:39:42 -0800

    edits

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -221,12 +221,13 @@ def train_multitask(args):
 
             optimizer.zero_grad()
             logit = model.predict_paraphrase(b_ids, b_mask, b_ids2, b_mask2)
-            tensor_a = b_labels.view(-1).type(torch.FloatTensor)
+            #tensor_a = b_labels.view(-1).type(torch.FloatTensor)
+            #tensor_a = b_labels.view(-1).float()
             print("made it")
-            tensor_b = logit.view(-1)
-            tensor_a = tensor_a.to(device)
+            #tensor_b = logit.view(-1)
+            #tensor_a = tensor_a.to(device)
             print("made it here")
-            loss = F.cross_entropy(tensor_b, tensor_a, reduction='sum') / args.batch_size
+            loss = F.cross_entropy(logit.view(-1), b_labels.view(-1).float(), reduction='sum') / args.batch_size
             print("made it to the first cross entropy")
             loss.backward()
             #loss = loss.to(model.device)
@@ -250,12 +251,12 @@ def train_multitask(args):
 
             optimizer.zero_grad()
             logit = model.predict_similarity(b_ids, b_mask, b_ids2, b_mask2)
-            tensor_b = logit.view(-1)
-            print("made it to the second")
-            tensor_a = b_labels.view(-1).type(torch.FloatTensor)
-            tensor_a = tensor_a.to(device)
-            print("made it to the second to device")
-            loss = F.cross_entropy(tensor_b, tensor_a, reduction='sum') / args.batch_size
+            #tensor_b = logit.view(-1)
+            #print("made it to the second")
+            #tensor_a = b_labels.view(-1).type(torch.FloatTensor)
+            #tensor_a = tensor_a.to(device)
+            #print("made it to the second to device")
+            loss = F.cross_entropy(logit.view(-1), b_labels.view(-1).float(), reduction='sum') / args.batch_size
 
             loss.backward
             optimizer.step()
