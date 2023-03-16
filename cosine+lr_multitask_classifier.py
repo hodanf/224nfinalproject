9f14c325174eff4666e4ea4eb930cfa052e726f6
simrink <simrinkalkat@gmail.com>
2023-03-16 15:23:19 -0700

    lr

--- cosine+lr_multitask_classifier.py
+++ cosine+lr_multitask_classifier.py
@@ -338,13 +338,13 @@ def get_args():
     parser.add_argument("--batch_size", help='sst: 64, cfimdb: 8 can fit a 12GB GPU', type=int, default=16)
     parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
     parser.add_argument("--lr", type=float, help="learning rate, default lr for 'pretrain': 1e-3, 'finetune': 1e-5",
-                        default=1e-5)
+                        default=1e-3)
     
     # parameters for extension 1: layer-wise learning rate decay
     parser.add_argument("--layer_learning_rate",
                         type=float,
                         nargs='+',
-                        default=[2e-5] * 12,
+                        default=[1.5e-5],
                         help="learning rate in each group")
     parser.add_argument("--layer_learning_rate_decay",
                         type=float,
