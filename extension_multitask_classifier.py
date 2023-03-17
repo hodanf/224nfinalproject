d03c74f2b49bd16379f87ca245eddd8ca9c5fc84
yvonne-hong <73502078+yvonne-hong@users.noreply.github.com>
2023-03-17 15:15:24 -0700

    added layer

--- extension_multitask_classifier.py
+++ extension_multitask_classifier.py
@@ -55,12 +55,11 @@ class MultitaskBERT(nn.Module):
         self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
         # self.sentiment_classifier = torch.nn.Linear(BERT_HIDDEN_SIZE, N_SENTIMENT_CLASSES)
         # self.sentiment_classifier_layer2 = torch.nn.Linear(BERT_HIDDEN_SIZE, N_SENTIMENT_CLASSES)
-        self.py_sequential = torch.nn.Sequential(torch.nn.Linear(BERT_HIDDEN_SIZE, BERT_HIDDEN_SIZE),
-                                                 torch.nn.Linear(BERT_HIDDEN_SIZE, N_SENTIMENT_CLASSES))
+        self.py_sequential = torch.nn.Sequential(torch.nn.Linear(BERT_HIDDEN_SIZE, (BERT_HIDDEN_SIZE + N_SENTIMENT_CLASSES)/2), torch.nn.Linear((BERT_HIDDEN_SIZE + N_SENTIMENT_CLASSES)/2, N_SENTIMENT_CLASSES))
         self.paraphrase_classifier = torch.nn.Linear(BERT_HIDDEN_SIZE * 2, 1)
         self.similarity = torch.nn.Linear(BERT_HIDDEN_SIZE * 2, 1) # read paper
-        self.similarity_projection_layer1 = torch.nn.Linear(BERT_HIDDEN_SIZE, BERT_HIDDEN_SIZE)
-        self.similarity_projection_layer2 = torch.nn.Linear(BERT_HIDDEN_SIZE, BERT_HIDDEN_SIZE)
+        self.similarity_projection_layer1 = torch.nn.Linear(BERT_HIDDEN_SIZE, BERT_HIDDEN_SIZE / 2)
+        self.similarity_projection_layer2 = torch.nn.Linear(BERT_HIDDEN_SIZE, BERT_HIDDEN_SIZE / 2)
 
 
     def forward(self, input_ids, attention_mask):
